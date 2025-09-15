0cc319f965f1e8cd108e4dbb5be011769b4838bd
SPRC\ljyepes <ljyepes@sprc.com.co>
2025-09-15 15:27:13 -0500

    code log

--- src/main/java/com/gpc/whatsapp/api/core/controllers/WhatsAppWebhookController.java
+++ src/main/java/com/gpc/whatsapp/api/core/controllers/WhatsAppWebhookController.java
@@ -4,10 +4,13 @@
  */
 package com.gpc.whatsapp.api.core.controllers;
 
+import com.gpc.whatsapp.api.core.model.response.Change;
 import com.gpc.whatsapp.api.core.model.response.Message;
+import com.gpc.whatsapp.api.core.model.response.Value;
 import com.gpc.whatsapp.api.core.model.response.WebhookEvent;
 import com.gpc.whatsapp.api.core.model.response.WhatsAppWebhook;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,44 +19,53 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
- 
+
 @RestController
 @RequestMapping("/webhook/whatsapp")
 public class WhatsAppWebhookController {
- 
+
 //    @Autowired
 //    private RegistroTurnoRepository registroRepo;
- 
     private static final String VERIFY_TOKEN = "misuperclave"; // Defínelo igual al de Meta
- 
+
     @GetMapping
     public String verifyWebhook(
             @RequestParam(name = "hub.mode", required = false) String mode,
             @RequestParam(name = "hub.verify_token", required = false) String token,
             @RequestParam(name = "hub.challenge", required = false) String challenge) {
- 
+
         if ("subscribe".equals(mode) && VERIFY_TOKEN.equals(token)) {
             return challenge; // ✅ Responder challenge si el token es correcto
         } else {
             return "Error: token inválido";
         }
     }
-    
+
     @PostMapping
     public String recibirMensaje(@RequestBody WebhookEvent webhook) {
-         System.out.println("Mensaje recibido"+ webhook.toString());
-        if (webhook.getEntry() != null && !webhook.getEntry().isEmpty()) {
-
-            Message message = webhook.getEntry().get(0).getChanges().get(0).getValue().getMessages().get(0);
-            String from = message.getFrom();
-            String body = message.getText().getBody();
-
-            System.out.println("Mensaje recibido de " + from + ": " + body);
-            enviarTexto(from, "hola mundo");
+        System.out.println("Mensaje recibido" + webhook.toString());
+        if (webhook.getEntry() == null || webhook.getEntry().isEmpty()) {
+            return "XX";
         }
-       return "OK";
+        List<Change> changes = webhook.getEntry().get(0).getChanges();
+        if (changes == null || changes.isEmpty()) {
+            return "XX";
+        }
+        List<Message> messages = changes.get(0).getValue().getMessages();
+        if (messages == null || messages.isEmpty()) {
+            return "XX";
+        }
+
+        Message message = messages.get(0);
+        String from = message.getFrom();
+        String body = message.getText().getBody();
+
+        System.out.println("Mensaje recibido de " + from + ": " + body);
+        enviarTexto(from, "hola mundo"+ body);
+
+        return "OK";
     }
-    
+
     public void enviarTexto(String to, String mensaje) {
         try {
             String url = "https://graph.facebook.com/v23.0/728695723670491/messages";
@@ -75,7 +87,8 @@ public class WhatsAppWebhookController {
 
             HttpEntity<Map<String, Object>> request = new HttpEntity<>(body, headers);
 
-            ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
+            ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class
+            );
 
             System.out.println("Respuesta de Meta: " + response.getBody());
 
@@ -84,10 +97,3 @@ public class WhatsAppWebhookController {
         }
     }
 }
-
-
-
-
-
-
-
